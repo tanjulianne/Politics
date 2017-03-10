3722682e4de08ee6324f92cf93368b7731ae9c45
Julianne Tan <Julianne Tan>
2017-03-11 02:03:15 +0800

    Edited Stemmer

--- Stemmer.py
+++ Stemmer.py
@@ -1,32 +1,44 @@
+import re
 import nltk, xml.dom.minidom
 from xml.dom.minidom import Node
 from nltk.corpus import stopwords
-from nltk.stem import *
+from nltk.stem.wordnet import WordNetLemmatizer
 
 #read file
 dom = xml.dom.minidom.parse("RodrigoDuterte.xml")
 Topic = dom.getElementsByTagName('article')
 
-raw = ""
-
+articles = ""
 for node in Topic:
 	alist = node.getElementsByTagName('body')
 	for a in alist:
 		para = a.getElementsByTagName('p')
 		for p in para:
 			article = p.firstChild.data
-			raw = raw + article
+			articles = articles + article
 
 #tokenize
 
-tokens = nltk.word_tokenize(raw)
+#tokens = []
+#for article in articles:
+#	tokens.extend(nltk.word_tokenize(article))
+
+tokens = re.findall(r"\w+",articles)
+
 #stem
 
-roots = ""
-stemmer = PorterStemmer()
+roots = []
+lmtzr = WordNetLemmatizer()
 for token in tokens:
-	roots = roots + stemmer.stem(token)
-print(' '.join(roots))
+	roots.extend(lmtzr.lemmatize(token))
+
+print(roots)
+
+#roots = ""
+#stemmer = PorterStemmer()
+#for token in tokens:
+#	roots = roots + stemmer.stem(token)
+#print(' '.join(roots))
 #roots = [stemmer.stem(tokens) for token in tokens]
 #filtered = [root for root in roots if root not in stopwords.words('english')]
 #print(' '.join(roots))
